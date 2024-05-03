25067aa7d101772e64f357292b959645b362bc7d
Irguin Alonso More <irguinalonsomore@gmail.com>
2024-05-02 19:58:15 -0500

    pantalla de publicaciones

--- app/src/main/java/com/example/scripters_society/publicar.java
+++ app/src/main/java/com/example/scripters_society/publicar.java
@@ -1,6 +1,12 @@
 package com.example.scripters_society;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class publicar extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,38 @@ public class publicar extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView imageButton = findViewById(R.id.btncamera);
+
+
+
+        // Establece un OnClickListener para el botón de la imagen
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llama al método para iniciar la captura de imagen
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Verifica si hay una aplicación de cámara disponible para manejar el intent
+        startActivityIfNeeded(intent, 1);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = findViewById(R.id.imgpublic);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imgBitmap);
+        }
+    }
+
+
 }
