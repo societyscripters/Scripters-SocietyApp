f5ce6399ac90c8b44b0cafee4db773848205ee8b
Irguin Alonso More <irguinalonsomore@gmail.com>
2024-05-11 09:56:32 -0500

    publicar galeria

--- app/src/main/java/com/example/scripters_society/publicar.java
+++ app/src/main/java/com/example/scripters_society/publicar.java
@@ -2,8 +2,10 @@ package com.example.scripters_society;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -54,15 +56,32 @@ public class publicar extends AppCompatActivity {
 
     }
 
+
+
+
+    public void onclick(View view) {
+        cargarImagen();
+    }
+    private void  cargarImagen(){
+        Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"seleccione la aplicacion"),10);
+    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        ImageView imageView = findViewById(R.id.imgpublic);
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+        if (requestCode == 10 && resultCode == RESULT_OK) {
+            Uri path = data.getData();
+            ImageView imageView = findViewById(R.id.imgpublic);
+            imageView.setImageURI(path);
+
+            // Imprime un mensaje en la consola
+
+        }else if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imgBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.imgpublic);
             imageView.setImageBitmap(imgBitmap);
         }
     }
-
-
 }
